08860fd3cc26890f9373fcaf285fe8b155390091
KaTZWorlD <kstewartjr77@gmail.com>
2023-10-14 15:46:02 -0400

    Update app.js

--- API/app.js
+++ API/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const data = require('./current.json');
+const data = require('current.json');
 
 app.get('/',(req,res) => {
         res.send("welcome to the world")
