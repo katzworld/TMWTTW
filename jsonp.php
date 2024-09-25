2ac0697fa3795f948a76b8f8ec88635b51776d6d
KaTZWorlD <kstewartjr77@gmail.com>
2024-09-24 23:11:52 -0400

    Create jsonp.php
    hosting locally

---
+++ jsonp.php
@@ -0,0 +1,8 @@
+<?php
+$api = $_GET["api"];
+if (!preg_match('/^http/i', $api)) exit;
+$callback = $_GET["callback"];
+header('Content-Type: application/javascript');
+$data = file_get_contents($api);
+echo $callback."(".$data.")";
+?>
