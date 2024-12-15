b5ef19460ebc687e55a68c039d1cf080a90c2b5a
KaTZWorlD <kstewartjr77@gmail.com>
2024-12-14 23:36:00 -0500

    Update jsonp.php

--- jsonp.php
+++ jsonp.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
 <?php
 $api = $_GET["api"];
 if (!preg_match('/^http/i', $api)) exit;
@@ -9,5 +6,3 @@ header('Content-Type: application/javascript');
 $data = file_get_contents($api);
 echo $callback."(".$data.")";
 ?>
-</body>
-</html>
